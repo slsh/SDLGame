aa265c0f79913155d920ea5430fd6123843732fc
Johan Eriksson <slashet@gmail.com>
2017-01-26 13:23:46 +0100

    Init SDL

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by johan eriksson on 26/01/2017.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+
+int main( int argc, char* args[] )
+{
+    //Init
+
+    return 0;
+}
