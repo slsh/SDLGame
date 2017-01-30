a47ca3954de21c58e47c43709b0f7cda22521c35
Niklas Larsson <nlarsson0@gmail.com>
2017-01-30 14:29:53 +0100

    Forgot header for TPiece

---
+++ headers/Pieces/TPiece.h
@@ -0,0 +1,17 @@
+//
+// Created by larsson on 2017-01-30.
+//
+
+#include <iostream>
+#include "../Piece.h"
+
+class TPiece:public Piece {
+public:
+    TPiece();
+    ~TPiece();
+    std::vector<std::vector<int>> getNextRotation();
+    std::vector<std::vector<int>> getLastRotation();
+    std::vector<std::vector<int>> getThisRotation();
+    void rotateLeft();
+    void rotateRight();
+};
