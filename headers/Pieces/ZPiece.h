5ac34d0b99145d6cd4a70e28f579bff0d109925d
Johan Eriksson <slashet@gmail.com>
2017-01-31 11:45:42 +0100

    adding all pieces

---
+++ headers/Pieces/ZPiece.h
@@ -0,0 +1,15 @@
+//
+// Created by larsson on 2017-01-30.
+//
+#ifndef SDLGAME_TPIECE_H
+#define SDLGAME_TPIECE_H
+
+#include "Piece.h"
+
+class TPiece:public Piece {
+public:
+    TPiece();
+    ~TPiece(); // This has to be declared
+};
+
+#endif //SDLGAME_TPIECE_H
